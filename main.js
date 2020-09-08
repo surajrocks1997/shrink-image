9f56e6cdb0a3c8299d9888a8c0734b02940d8d92
surajrocks1997 <surajrocks1997@gmail.com>
2020-09-08 13:18:30 +0530

    using customized menu options and global shortcuts

--- main.js
+++ main.js
@@ -1,20 +1,70 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
+
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
 
-function createMainWindow() {
+const createMainWindow = () => {
   mainWindow = new BrowserWindow({
     webPreferences: {
       worldSafeExecuteJavaScript: true,
     },
-    width: 500,
-    height: 600,
+    width: 1000,
+    height: 800,
     title: 'ImageShrink',
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    backgroundColor: 'white'
   });
 
   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile('./app/index.html');
-}
+};
 
-app.on('ready', createMainWindow);
+app.on('ready', () => {
+  createMainWindow();
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
+    mainWindow.toggleDevTools()
+  );
+
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+        accelerator: 'CmdOrCtrl+W',
+        click: () => app.quit(),
+      },
+    ],
+  },
+];
+
+app.on('window-all-closed', () => {
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
